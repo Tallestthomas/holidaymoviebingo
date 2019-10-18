a729eda875c7a621a8497f1abde5f2bcd2c25f6b
Tom Rasmussen <info@tomrasmussen.me>
2019-10-18 18:46:05 -0400

    moving files and updating deps

---
+++ src/components/Square.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Square = ({ children }) => {
+  const [selected, setSelected] = useState(false);
+
+  return(
+    <div className={`square ${selected ? 'selected' : ''}`} 
+      onClick={() => setSelected(!selected)}>
+      { children }
+    </div>
+  );
+}
+
+export default Square;
