2056093d07103cd4e0ef5f34c994e5a84cbfe562
Tom Rasmussen <trasmussen@c02w22bghtdd.home>
2020-11-22 17:02:10 -0500

    updates free square

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
-import React, { Component } from 'react';
-import Square from './components/Square';
-import { squares } from './data/squares.json';
-import './App.css'
+import React, { Component } from "react";
+import Square from "./components/Square";
+import { squares } from "./data/squares.json";
+import "./App.css";
 
 class App extends Component {
   state = {
-    squaresToRender: []
-  }
+    squaresToRender: [],
+  };
 
-  shuffleArray = (array)  => {
-    this.setState({squaresToRender: array.sort(() => 0.5 - Math.random())});
-  }
+  shuffleArray = (array) => {
+    this.setState({ squaresToRender: array.sort(() => 0.5 - Math.random()) });
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.shuffleArray(squares);
   }
 
   render() {
     const { squaresToRender } = this.state;
 
-    return(
+    return (
       <div className="app">
         <img src={require("./assets/image.jpeg")} alt="logo" />
-        {squaresToRender ?
-        <div className="square-container">
-          { squaresToRender.map((square, index) => {
-            if( index === 12 )
-              return <div className="square selected">Free Space</div>
-                if(index < 25)
-              return <Square>{square}</Square>
-                return null;
-          }) }
-        </div>
-            :
-            <span>loading...</span>
-        }
+        {squaresToRender ? (
+          <div className="square-container">
+            {squaresToRender.map((square, index) => {
+              if (index === 12)
+                return <div className="square selected">Terrible Acting</div>;
+              if (index < 25) return <Square>{square}</Square>;
+              return null;
+            })}
+          </div>
+        ) : (
+          <span>loading...</span>
+        )}
       </div>
-    )
+    );
   }
 }
 
